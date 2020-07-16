bef6b7475fed19da086a4ef59a3f40b2d3cc01dd
Nguyễn Văn Minh <minhnv2306@gmail.com>
2020-07-16 21:24:20 +0700

    fix: change Dockerfile

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/sh -l
 
 echo "Hello $1"
+service ssh start
 
 mkdir ~/.ssh
 touch ~/.ssh/id_rsa
@@ -8,21 +9,9 @@ touch ~/.ssh/known_hosts
 
 echo "$2" > ~/.ssh/id_rsa
 
-echo "Host 52.15.170.75
-    HostName 52.15.170.75
-    User ubuntu
-    IdentityFile ~/.ssh/id_rsa" > ~/.ssh/config
-ssh-keyscan 52.15.170.75 > ~/.ssh/known_hosts
-
-cat ~/.ssh/config
-cat ~/.ssh/known_hosts
+echo "$3" > ~/.ssh/known_hosts
 
 chmod 600 ~/.ssh/id_rsa
-chmod 644 ~/.ssh/config
 chmod 644 ~/.ssh/known_hosts
 
-ssh -o StrictHostKeyChecking=no -T ubuntu@52.15.170.75
-
-cd ~/.ssh
-
-ls -la
+ssh -o StrictHostKeyChecking=no ubuntu@52.15.170.75
